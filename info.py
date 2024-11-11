3cb3d7ceba851f8d8b9b4eda7ec92257b8779653
rajbro779988 <rajbro1211@gmail.com>
2024-11-12 01:06:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 
 
 ADMIN = int(getenv("ADMIN", "1393092521"))
-SILICON_PIC = os.environ.get("SILICON_PIC", "https://i.ibb.co/tKTKd2G/photo-2024-11-11-19-11-10-7436101514944315408.jpg")
+SILICON_PIC = os.environ.get("SILICON_PIC", "https://i.ibb.co/kX2mc0h/photo-2024-11-11-19-35-08-7436107579438137364.jpg")
 API_ID = int(getenv("API_ID", "14680661"))
 API_HASH = str(getenv("API_HASH", "166f6e394021081c5cdb41c92344deb7"))
 BOT_TOKEN = str(getenv("BOT_TOKEN", "7933899088:AAELTXuyGTHPdrK6zuEYMRgMOqEZWP3CG6Y"))
