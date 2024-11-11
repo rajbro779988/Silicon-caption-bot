4c09e44ec8cf6277370d0119706d7db52ac591d7
Raj Verma <118580735+Rajsona123@users.noreply.github.com>
2024-11-12 00:46:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,13 +14,13 @@ def is_enabled(value, default):
         return default
 
 
-ADMIN = int(getenv("ADMIN", ""))
-SILICON_PIC = os.environ.get("SILICON_PIC", "https://telegra.ph/file/21a8e96b45cd6ac4d3da6.jpg")
-API_ID = int(getenv("API_ID", ""))
-API_HASH = str(getenv("API_HASH", ""))
-BOT_TOKEN = str(getenv("BOT_TOKEN", ""))
-FORCE_SUB = os.environ.get("FORCE_SUB", "") 
-MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://replacewithyourmongodb:replacewithyourmongodb@cluster0.zi78j51.mongodb.net/?retryWrites=true&w=majority",))
+ADMIN = int(getenv("ADMIN", "1393092521"))
+SILICON_PIC = os.environ.get("SILICON_PIC", "https://i.ibb.co/tKTKd2G/photo-2024-11-11-19-11-10-7436101514944315408.jpg")
+API_ID = int(getenv("API_ID", "14680661"))
+API_HASH = str(getenv("API_HASH", "166f6e394021081c5cdb41c92344deb7"))
+BOT_TOKEN = str(getenv("BOT_TOKEN", "7933899088:AAELTXuyGTHPdrK6zuEYMRgMOqEZWP3CG6Y"))
+FORCE_SUB = os.environ.get("FORCE_SUB", "sb_botz_update") 
+MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://raj94626:autocaptionbot@cluster0.wqaujtm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",))
 DEF_CAP = str(
     getenv(
         "DEF_CAP",
